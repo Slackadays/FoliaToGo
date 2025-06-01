eaa082a1bb95e60afefef55d2e39835a13f2b02a
Netscape7 <66892116+Netscape7@users.noreply.github.com>
2025-06-02 05:38:20 +0900

    Fixed build errors and the path of the Jar file in the build result. (#4)
    * Update build.sh
    
    * Update folia.yml

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ git clone https://github.com/PaperMC/Folia
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
-./gradlew applyPatches
-./gradlew createReobfPaperclipJar
+./gradlew applyAllPatches
+./gradlew createMojmapPaperclipJar
